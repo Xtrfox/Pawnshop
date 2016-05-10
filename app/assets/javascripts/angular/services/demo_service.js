f0a9433c8d333d73162c738b19bf606d5fde84a2
Life Christian <lifelofranco@gmail.com>
2016-05-10 13:15:00 +0800

    customer form

--- app/assets/javascripts/angular/services/demo_service.js
+++ app/assets/javascripts/angular/services/demo_service.js
@@ -15,40 +15,48 @@ angular.module('Pawn.services')
           "date_added": "06/15/2014"
         },
         {
-          "id":2,
-          "customer_id":"32199",
-          "department": "Legal",
-          "first_name":"Francis",
-          "last_name":"Plaza",
-          "perk_used":"34",
-          "date_added":"06/25/2014"
+          "id":1,
+          "customer_id":"3579",
+          "first_name":"Paolo",
+          "last_name":"Agloro",
+          "city": "Mandaluyong",
+          "mobile": "09997774433",
+          "landline": "4448899",
+          "birth_date": "January 1, 1960",
+          "date_added": "06/15/2014"
         },
         {
-          "id":3,
-          "customer_id":"123459",
-          "department": "R and D",
-          "first_name":"Life",
+          "id":1,
+          "customer_id":"3579",
+          "first_name":"Life Lofranco",
           "last_name":"Lofranco",
-          "perk_used":"10",
-          "date_added":"07/10/2014"
+          "city": "Mandaluyong",
+          "mobile": "09997774433",
+          "landline": "4448899",
+          "birth_date": "January 1, 1960",
+          "date_added": "06/15/2014"
         },
         {
-          "id":4,
-          "customer_id":"455554",
-          "department": "Sales and Marketing",
-          "first_name":"Bryan",
-          "last_name":"Conde",
-          "perk_used":"54",
-          "date_added":"07/10/2014"
+          "id":1,
+          "customer_id":"3579",
+          "first_name":"Leo Lope",
+          "last_name":"Lofranco",
+          "city": "Mandaluyong",
+          "mobile": "09997774433",
+          "landline": "4448899",
+          "birth_date": "January 1, 1960",
+          "date_added": "06/15/2014"
         },
         {
-          "id":5,
-          "customer_id":"445221",
-          "department": "Admin",
-          "first_name":"Carlos",
-          "last_name":"Celdran",
-          "perk_used":"43",
-          "date_added":"07/10/2014"
+          "id":1,
+          "customer_id":"3579",
+          "first_name":"Leo Lope",
+          "last_name":"Lofranco",
+          "city": "Mandaluyong",
+          "mobile": "09997774433",
+          "landline": "4448899",
+          "birth_date": "January 1, 1960",
+          "date_added": "06/15/2014"
         }
       ];
     };
